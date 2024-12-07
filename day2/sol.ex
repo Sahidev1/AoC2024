8ec62f83fe8edadc00033ff7b5d4a6414f7f7b96
Sahidev1 <theoddrogue@gmail.com>
2024-12-07 20:14:36 +0100

    day2 part2 complete

--- day2/sol.ex
+++ day2/sol.ex
@@ -1,10 +1,30 @@
 defmodule Sol do
   @type levelorder :: nil | :positive | :negative
 
-  def main do
+  def part1 do
     input = parseInput()
-    proc(input)
+    proc1(input)
   end
+
+  def part2 do
+    input = parseInput()
+    cnt = length(input)
+    IO.puts("count is: #{cnt}")
+    unsafeReps = Enum.filter(input, fn e->
+      is_safe(e, nil) === 0
+    end)
+    cnt = length(unsafeReps)
+    IO.puts("count is: #{cnt}")
+
+    proc1(input) + proc2(unsafeReps, 0)
+
+  end
+
+  def getDiffs(rep=[curr|rest=[next|tail]], acc) do
+    acc = acc++[curr - next]
+    getDiffs(rest, acc)
+  end
+  def getDiffs([curr|_], acc) do acc end
 
   def example do
     input = [
@@ -13,13 +33,34 @@ defmodule Sol do
       [7,5,5,1],
       [1,5,7,9]
     ]
-    proc(input)
+    unsafeReps = Enum.filter(input, fn e->
+      is_safe(e, nil) === 0
+    end)
+    IO.inspect(unsafeReps)
+    {proc1(input), proc2(unsafeReps, 0)}
   end
 
-  def proc([report | rest]) do
-    is_safe(report, nil) + proc(rest)
+  def proc2([], sum) do sum end
+  def proc2([report| rest], sum) do
+    safety = isActuallySafe(report,0, length(report) - 1, 0)
+    proc2(rest, sum + safety)
   end
-  def proc([]) do 0 end
+
+  def isActuallySafe(_,_,_,isSafe) when isSafe === 1 do 1 end
+  def isActuallySafe(report, i, maxIndex, isSafe) when i > maxIndex do isSafe end
+  def isActuallySafe(report, i, maxIndex, isSafe) do
+    subRep = List.delete_at(report, i)
+    safety = is_safe(subRep, nil)
+    #if(safety === 0) do IO.inspect(subRep) end
+    isActuallySafe(report, i + 1, maxIndex, safety)
+  end
+
+
+
+  def proc1([report | rest]) do
+    is_safe(report, nil) + proc1(rest)
+  end
+  def proc1([]) do 0 end
 
   def is_safe([curr | tail = [next | rest]], nil) do
     diff = curr - next
