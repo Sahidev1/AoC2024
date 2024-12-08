858a9dfeb1a47f20a5beda59068b4c52b990e574
Sahidev1 <theoddrogue@gmail.com>
2024-12-08 19:26:56 +0100

    day3 part 2 completed

--- day3/sol.ex
+++ day3/sol.ex
@@ -1,19 +1,45 @@
 
 defmodule Sol do
 
-  def main do
+  def part1 do
     input = parseInput()
     matches = Regex.scan(~r/mul\([0-9]{1,3},[0-9]{1,3}\)/, input)
-    proc(matches)
+    proc1(matches)
   end
 
-  def proc([[curr|_]|tail]) do
+  def part2 do
+    input = parseInput()
+    input = "do()"<>input<>"don't()"
+    input = String.replace(input, ~r/\s+/, "")
+
+    all = Regex.scan(~r/(?<=do\(\)).+?(?=don't\(\))/, input)|>Enum.map(fn [e|_]->e end)
+
+
+    sum = Enum.reduce(all, 0, fn str,acc ->
+      matches = Regex.scan(~r/mul\([0-9]{1,3},[0-9]{1,3}\)/, str)
+      acc + proc1(matches)
+    end)
+  end
+
+  def example2 do
+    input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+    input = input<>"don't()"
+    [first_enabled|_] = Regex.run(~r/.+?(?=don't\(\))/m, input)
+    rest = Regex.scan(~r/(?<=do\(\)).+?(?=don't\(\))/m, input)
+    all = [first_enabled| rest]
+    enabledInstructions = Enum.join(all)
+    matches = Regex.scan(~r/mul\([0-9]{1,3},[0-9]{1,3}\)/, enabledInstructions)
+    proc1(matches)
+  end
+
+
+  def proc1([[curr|_]|tail]) do
     [[a],[b]] = Regex.scan(~r/[0-9]+/, curr)
     a = String.to_integer(a)
     b = String.to_integer(b)
-    (a*b) + proc(tail)
+    (a*b) + proc1(tail)
   end
-  def proc([]) do 0 end
+  def proc1([]) do 0 end
 
 
   def parseInput do
